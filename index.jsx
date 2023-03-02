2abc1299b57231abe904d8be1620a76d9e2003df
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2023-03-02 20:09:02 +0100

    feat(kollab): add files

---
+++ index.jsx
@@ -0,0 +1,260 @@
+<root>
+  <node
+    position={{ x: 407.6245422363281, y: 590.38623046875, z: -100.54539489746094 }}
+    orientation={{ w: 0.5709474682807922, x: 0.16907577216625214, y: 0.7558804154396057, z: -0.27217140793800354 }}>
+    <light
+      {...{ lighttype: 'point' }}
+      direction={{ x: 0, y: -1, z: 0 }}
+      diffuse={{ r: 1.0, g: 1.0, b: 1.0 }}
+      specular={{ r: 1.0, g: 1.0, b: 1.0 }}>
+    </light>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cube.015_Cube.021.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cube.015_Cube.021_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cylinder.003.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cylinder.003_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cylinder.002.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cylinder.002_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cylinder.001.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cylinder.001_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cube.014_Cube.020.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cube.014_Cube.020_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cube.013_Cube.019.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cube.013_Cube.019_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cube.012_Cube.018.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cube.012_Cube.018_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cube.011_Cube.017.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cube.011_Cube.017_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cube.010_Cube.016.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cube.010_Cube.016_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cube.008_Cube.014.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cube.008_Cube.014_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cube.009_Cube.015.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cube.009_Cube.015_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cube.006_Cube.012.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cube.006_Cube.012_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cube.007_Cube.013.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cube.007_Cube.013_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cube.004_Cube.010.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cube.004_Cube.010_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cube.005_Cube.011.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cube.005_Cube.011_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cube.003_Cube.009.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cube.003_Cube.009_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cylinder.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cylinder_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cube.001_Cube.006.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cube.001_Cube.006_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'Cube.002_Cube.007.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'Cube.002_Cube.007_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'pdloga_Cube.005.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'pdloga_Cube.005_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'na_szybie_Plane.002.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'na_szybie_Plane.002_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'szyba_Plane.001.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'szyba_Plane.001_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'okna_drewno_Cube.004.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'okna_drewno_Cube.004_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'framuga_Cube.003.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'framuga_Cube.003_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'lewa_sciana_Cube.002.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'lewa_sciana_Cube.002_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'prawa_sciana_Cube.001.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'prawa_sciana_Cube.001_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 0.7071067094802856, x: 0.7071068286895752, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 0.9999999403953552, z: 0.9999999403953552 }}>
+    <mesh
+      {...{ url: 'sciana_okno_Cube.mesh' }}
+      physical={{ raycast: true, shapes: [{'shape': 'mesh', 'mass': 1, 'offset': {'x': 0, 'y': 0, 'z': 0}, 'rotation': {'w': 1, 'x': 0, 'y': 0, 'z': 0}, 'mesh': 'sciana_okno_Cube_phy.obj'}] }}>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 0.0, y: 0.0, z: -0.0 }}
+    orientation={{ w: 1.0, x: 0.0, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+  </node>
+</root>
